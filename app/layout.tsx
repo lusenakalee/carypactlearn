65ac3813bcd9fc85c1f1e47e8b6c93cc638dcaaf
lusenakalee <lusenakalee@gmail.com>
2026-09-12 18:00:27 +0300

    analytics update

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import LocaleProvider from '@/components/LocaleProvider';
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
@@ -31,6 +32,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
               <LocaleProvider>
         {children}
         </LocaleProvider>
+        <Analytics/>
         </body>
     </html>
   );
